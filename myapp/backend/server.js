c533e68820793a4480a50b3713c83bbbc925d213
n3e0t8 <66070308@kmitl.ac.th>
2025-10-31 11:11:19 +0700

    try to implement upload image V.5

--- myapp/backend/server.js
+++ myapp/backend/server.js
@@ -10,8 +10,9 @@ dotenv.config();
 const app = express();
 const PORT = 5000;
 app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(bodyParser.json({ limit: "10mb" }));
+
 
 const upload = multer({ storage: multer.memoryStorage() });
 
